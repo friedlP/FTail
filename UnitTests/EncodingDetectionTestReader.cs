eb7e856610d5baef0752befa47100f8c999ba916
friedlP <friedrich.priewasser@gmail.com>
2017-10-03 20:14:40 +0200

    Added helper class for unit tests

---
+++ UnitTests/EncodingDetectionTestReader.cs
@@ -0,0 +1,36 @@
+﻿using FastFileReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UnitTests {
+   class EncodingDetectionTestReader : EncodingDetectionReader {
+      bool validationRequired;
+
+      public MemoryStream Stream { get; } = new MemoryStream();
+
+      protected override Stream GetStream() {
+         return Stream;
+      }
+
+      public void SetValidationRequired() {
+         validationRequired = true;
+      }
+
+      protected override void EncodingValidated() {
+         validationRequired = false;
+      }
+
+      protected override bool EncodingValidationRequired() {
+         return validationRequired;
+      }
+
+      protected override void Reset() {
+         base.Reset();
+         validationRequired = false;
+      }
+   }
+}
