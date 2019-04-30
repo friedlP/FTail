3583a5223517b2630054edad511379ac05a279df
friedlP <friedrich.priewasser@gmail.com>
2019-04-30 23:31:25 +0200

    EncodingDetectionReader.CachedReader: Performance optimizations

--- FastFileReader/EncodingDetectionReader.CachedReader.cs
+++ FastFileReader/EncodingDetectionReader.CachedReader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace FastFileReader
@@ -18,6 +19,44 @@ namespace FastFileReader
             this.feedDetector = feedDetector;
          }
 
+         private Line Find(List<Line> lines, long position)
+         {
+            int pos = Find(lines, (l) => l.Extent, position);
+            if (pos >= 0)
+               return lines[pos];
+            else
+               return null;
+         }
+
+         private Extent Find(List<Extent> extents, long position)
+         {
+            int pos = Find(extents, (e) => e, position);
+            if (pos >= 0)
+               return extents[pos];
+            else
+               return null;
+         }
+
+         private int Find<T>(List<T> elements, Func<T, Extent> toExtent, long position)
+         {
+            if (elements == null || elements.Count == 0 || position < toExtent(elements[0]).Begin || position >= toExtent(elements[elements.Count - 1]).End)
+               return -1;
+            int min = 0;
+            int max = elements.Count - 1;
+            while (min <= max)
+            {
+               int pos = min + (max - min) / 2;
+               Extent e = toExtent(elements[pos]);
+               if (e.Begin <= pos && e.End > pos)
+                  return pos;
+               else if (e.Begin > pos)
+                  max = pos - 1;
+               else
+                  min = pos + 1;
+            }
+            return -1;
+         }
+
          public Line Read(long position)
          {
             Line line = null;
@@ -29,25 +68,11 @@ namespace FastFileReader
                }
                else if (cache.PreviousLines.Count > 0 && cache.PreviousLines[0].Begin <= position && cache.PreviousLines[cache.PreviousLines.Count - 1].End > position)
                {
-                  foreach (Line l in cache.PreviousLines)
-                  {
-                     if (l.Begin <= position && l.End > position)
-                     {
-                        line = l;
-                        break;
-                     }
-                  }
+                  line = Find(cache.PreviousLines, position);
                }
                else if (cache.NextLines.Count > 0 && cache.NextLines[0].Begin <= position && cache.NextLines[cache.NextLines.Count - 1].End > position)
                {
-                  foreach (Line l in cache.NextLines)
-                  {
-                     if (l.Begin <= position && l.End > position)
-                     {
-                        line = l;
-                        break;
-                     }
-                  }
+                  line = Find(cache.PreviousLines, position);
                }
             }
             if (line != null && line.End < cache.StreamLength)
@@ -79,25 +104,11 @@ namespace FastFileReader
                }
                else if (cache.PreviousExtents.Count > 0 && cache.PreviousExtents[0].Begin <= position && cache.PreviousExtents[cache.PreviousExtents.Count - 1].End > position)
                {
-                  foreach (Extent e in cache.PreviousExtents)
-                  {
-                     if (e.Begin <= position && e.End > position)
-                     {
-                        extent = e;
-                        break;
-                     }
-                  }
+                  extent = Find(cache.PreviousExtents, position);
                }
                else if (cache.NextExtents.Count > 0 && cache.NextExtents[0].Begin <= position && cache.NextExtents[cache.NextExtents.Count - 1].End > position)
                {
-                  foreach (Extent e in cache.NextExtents)
-                  {
-                     if (e.Begin <= position && e.End > position)
-                     {
-                        extent = e;
-                        break;
-                     }
-                  }
+                  extent = Find(cache.NextExtents, position);
                }
             }
             if (extent != null && extent.End < cache.StreamLength)
